279d7be50c8d6efc0835a96895c25314b16b3f3a
Luis David Garcia Lunar <ldgl1215@gmail.com>
2022-02-14 09:04:38 -0300

    Add project files.

---
+++ TaskCRUD/Controllers/TasksController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskCRUD.Controllers
+{
+    [ApiController]
+    [Route("tasks")]
+    public class TasksController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public TasksController()
+        {
+        }
+        // localhost:5000/WeatherForecast METHOD: GET
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+        }
+
+    }
+}
---
+++ TaskCRUD/Model/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskCRUD.Model
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; } = false;
+    }
+}
